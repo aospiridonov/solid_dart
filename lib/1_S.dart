9995cbee834e9a53c6a018874268a106a6386aa2
aospiridonov <aospiridonov@gmail.com>
2020-04-09 15:49:12 +0300

    Init Single-responsibility principle

---
+++ lib/1_S.dart
@@ -0,0 +1,6 @@
+//Single-responsibility principle
+/*
+  A class should only have a single responsibility, that is, 
+  only changes to one part of the software's specification 
+  should be able to affect the specification of the class.
+*/
