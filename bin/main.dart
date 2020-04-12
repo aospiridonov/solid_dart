5e4c4cb3e36af2a050d832bab63712b3f805ec88
aospiridonov <aospiridonov@gmail.com>
2020-04-13 01:16:17 +0300

    refactoring in master

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,14 @@
 import 'package:solid_dart/1_S.dart' as S;
 
-void main(List<String> arguments) {
+void funcS(){
   var news = new S.News(title:"Single-responsibility principle", text:"A class should only have a single...");
   print(news);
   
   print(S.NewsPrinter(news).html());
   print(S.NewsPrinter(news).json());
   print(S.NewsPrinter(news).xml());
+}
+
+void main(List<String> arguments) {
+  funcS();
 }
