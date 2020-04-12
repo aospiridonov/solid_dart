fe3241a5954cfa4937a2e71fb1702ef2f8876dab
aospiridonov <aospiridonov@gmail.com>
2020-04-13 01:09:51 +0300

    Single responsibilirty

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,8 @@ import 'package:solid_dart/1_S.dart' as S;
 void main(List<String> arguments) {
   var news = new S.News(title:"Single-responsibility principle", text:"A class should only have a single...");
   print(news);
-  news.update("... modified");
-  print(news);
+  
+  print(S.NewsPrinter(news).html());
+  print(S.NewsPrinter(news).json());
+  print(S.NewsPrinter(news).xml());
 }
--- lib/1_S.dart
+++ lib/1_S.dart
@@ -5,12 +5,53 @@
   should be able to affect the specification of the class.
 */
 
+import 'dart:convert';
+
 class News{
   String title;
   String text;
   bool modified = false;
 
   News({this.title, this.text});
-  String toString() => ((!modified)?"Orign":"Modified") + ("\nTitle: ${title}\nText: ${text}");
+  String toString() => ("\nTitle: ${title}\nText: ${text}");
   void update(String text) => {this.text = text, this.modified = true};
+
+//not single responsibility
+  //String toJson() => "";
+  //String toXml() => "";
+  //to end more
+}
+
+class NewsPrinter {
+  News news;
+  NewsPrinter(this.news);
+  String html()
+  {
+    return ''' 
+      <div class="news">
+      <h1>${this.news.title}</h1>
+        <p>${this.news.text}</p>
+      </div>
+    ''';
+  }
+
+  String json()
+  {
+    return JsonEncoder().convert({
+      'title': this.news.title,
+      'text': this.news.text,
+      'modified' : this.news.modified
+    });
+  }
+
+  String xml()
+  {
+    return ''' 
+      <news>
+        <title>${this.news.title}</title>
+        <text>${this.news.text}</text>
+        <modified>${this.news.modified}</modified>
+      </news>
+    ''';
+  }
 }
