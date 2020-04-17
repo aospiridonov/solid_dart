2205e32b47c1b9ef641a79a2ee20791084b06a1c
aospiridonov <aospiridonov@gmail.com>
2020-04-17 22:11:48 +0300

    Add principle open closed

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'package:solid_dart/1_S.dart' as S;
+import 'package:solid_dart/2_O.dart' as O;
 
 void funcS(){
   var news = new S.News(title:"Single-responsibility principle", text:"A class should only have a single...");
@@ -9,6 +10,16 @@ void funcS(){
   print(S.NewsPrinter(news).xml());
 }
 
+void funcO(){
+  var areaCalculator = new O.AreaCalculator([
+    new O.Circle(10),
+    new O.Square(20),
+    new O.Triangle(1, 2, 3),
+    new O.Rectangle(5, 10) ]);
+  print("sum area = ${areaCalculator.sum()}");
+}
+
 void main(List<String> arguments) {
-  funcS();
+  //funcS();
+  funcO();
 }
--- lib/2_O.dart
+++ lib/2_O.dart
@@ -3,3 +3,53 @@
   Software entities ... should be open for extension,
   but closed for modification.
 */
+
+import 'dart:math' as math;
+
+abstract class Shape {
+  double area(); 
+}
+
+class Circle implements Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double area() => radius * radius * math.pi;
+}
+
+class Square implements Shape {
+  double size;
+
+  Square(this.size);
+
+  @override
+  double area() => size * size;
+}
+
+class Rectangle implements Shape {
+  double width;
+  double height;
+
+  Rectangle(this.width, this.height);
+
+  @override
+  double area() => width * height;
+}
+
+class Triangle implements Shape {
+  double a, b, c;
+  Triangle(this.a, this.b, this.c);
+
+  @override
+  double area() =>  a * b * c;
+}
+
+class AreaCalculator {
+  var shapes = [];
+  AreaCalculator(this.shapes);
+
+  double sum() => shapes.fold(0, (accumulator, shape) => accumulator + shape.area());
+}
+
